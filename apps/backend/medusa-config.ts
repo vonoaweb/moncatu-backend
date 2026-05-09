223b1955fb0b2539866c1d47086733d48e847d46
vonoaweb <hola@vonoaweb.com>
2026-05-09 13:10:22 -0600

    fix: CORS and admin settings

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -4,16 +4,16 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   admin: {
-    disable: process.env.NODE_ENV === "production" ? true : false,
+    disable: process.env.DISABLE_ADMIN === "true",
     backendUrl: process.env.MEDUSA_BACKEND_URL || "https://moncatu-backend-production.up.railway.app",
   },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS || "https://vonoaweb.github.io,http://localhost:8000,http://localhost:5500",
+      adminCors: process.env.ADMIN_CORS || "https://moncatu-backend-production.up.railway.app,http://localhost:9000,http://localhost:5173",
+      authCors: process.env.AUTH_CORS || "https://vonoaweb.github.io,https://moncatu-backend-production.up.railway.app,http://localhost:8000,http://localhost:9000",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
